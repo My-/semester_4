c56db7a34a2889eeb505a37f5cd6bc8401e213ae
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-01-22 16:32:21 +0000

    exercize 1-2

---
+++ MobileApp/lab_1/lab1.ts
@@ -0,0 +1,17 @@
+
+function addition(value: string){
+    console.log(`Value is: ${value}`)
+}
+
+const firstVal: number = 42
+const secondVal: number = 1
+let sumOfVals: string = (firstVal +secondVal).toLocaleString()
+
+addition(sumOfVals)
+
+// Exercise 1
+const strLength = (s: string) => s.length
+const word: string = 'hello'
+console.log(`Exercise 1: ${word} length is: ${strLength(word)}`)
+
+// Exercise 2
