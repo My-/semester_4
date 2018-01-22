ca5e7523f5dc5ca642b900ca093995a2bde37d4b
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-01-22 17:54:15 +0000

    added optional parameter functionality

--- MobileApp/lab_1/lab1.ts
+++ MobileApp/lab_1/lab1.ts
@@ -15,5 +15,15 @@ let word: string = 'By by c#'
 console.log(`Exercise 1: ${word} length is: ${strLength(word)}`)
 
 // Exercise 2
-const strLengthNoSpaces = (s: string) => s.replace(/ /g, "").length
+const strLengthNoSpaces = (s: string): number => s.replace(/ /g, "").length
 console.log(`Exercise 2: ${word} length without spaces is: ${strLengthNoSpaces(word)}`)
+
+// Exercise 3
+const countLetters =
+    (s:string, noSpaces?: boolean) => noSpaces ? strLengthNoSpaces(s) : strLength(s)
+
+console.log(`
+    Exercise 3:
+        string is: ${word},
+        lenght no spaces: ${countLetters(word, true)},
+        lenght with spaces ${countLetters(word)}`)
