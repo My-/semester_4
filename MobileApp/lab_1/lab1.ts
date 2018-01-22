4ce8ff5e4282e424e952fbe9a6892b4ddc3644f0
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-01-22 16:54:20 +0000

    comand line commit

--- MobileApp/lab_1/lab1.ts
+++ MobileApp/lab_1/lab1.ts
@@ -11,7 +11,9 @@ addition(sumOfVals)
 
 // Exercise 1
 const strLength = (s: string) => s.length
-const word: string = 'hello'
+let word: string = 'By by c#'
 console.log(`Exercise 1: ${word} length is: ${strLength(word)}`)
 
 // Exercise 2
+const strLengthNoSpaces = (s: string) => s.replace(/[ ]/, "").length
+console.log(`Exercise 2: ${word} length without spaces is: ${strLengthNoSpaces(word)}`)
