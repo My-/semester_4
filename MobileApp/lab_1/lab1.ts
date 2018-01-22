83da20c25832bd75629d0c75284a15618b4cf2dd
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-01-22 17:10:08 +0000

    exercise 2 done

--- MobileApp/lab_1/lab1.ts
+++ MobileApp/lab_1/lab1.ts
@@ -15,5 +15,5 @@ let word: string = 'By by c#'
 console.log(`Exercise 1: ${word} length is: ${strLength(word)}`)
 
 // Exercise 2
-const strLengthNoSpaces = (s: string) => s.replace(/[ ]/, "").length
+const strLengthNoSpaces = (s: string) => s.replace(/ /g, "").length
 console.log(`Exercise 2: ${word} length without spaces is: ${strLengthNoSpaces(word)}`)
